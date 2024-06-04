4cbee1708e4a839df4d16b7b529f21f095b18191
Tomer Klein <tomer.klein@gmail.com>
2024-06-04 09:10:56 +0300

    Adding title to the whatsapp message

--- redalert.py
+++ redalert.py
@@ -107,7 +107,7 @@ def alarm_on(data):
     try:
         if GREEN_API_INSTANCE and GREEN_API_TOKEN:
             greenAPI = API.GreenAPI(GREEN_API_INSTANCE, GREEN_API_TOKEN)
-            greenAPI.sending.sendMessage(WHATSAPP_NUMBER, 'באזורים הבאים: \r\n ' + ', '.join(data["data"]).replace(',','\r\n') + '\r\n' + str(data["desc"] ))
+            greenAPI.sending.sendMessage(WHATSAPP_NUMBER,f"* {str(data["title"])} * \n " + 'באזורים הבאים: \r\n ' + ', '.join(data["data"]).replace(',','\r\n') + '\r\n' + str(data["desc"] ))
     except Exception as e:
         logger.error(f"Error sending whatsapp message. \n {str(e)}")
 
