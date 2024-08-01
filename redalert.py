1575a9d1636df0279b8772f662cecf3a9609b0f1
Tomer Klein <tomer.klein@gmail.com>
2024-08-01 10:15:54 +0300

    Bump to version 3.3.0 and added areas to the alert message

--- redalert.py
+++ redalert.py
@@ -4,6 +4,8 @@ import threading
 import paho.mqtt.client as mqtt
 import urllib3
 import os
+import requests
+import re
 from loguru import logger
 import time
 import codecs
@@ -100,7 +102,9 @@ def alarm_on(data):
     client.publish(MQTT_TOPIC,'on',qos=0,retain=False)
     title=str(data["title"])
     wapp_title = f"*{title}*"
-    body='באזורים הבאים: \r\n ' + ', '.join(data["data"]).replace(',','\r\n') + '\r\n' + str(data["desc"] )
+    categorized_places = categorize_places(lamas, data["data"])
+    places = format_output(categorized_places)
+    body='באזורים הבאים: \r\n ' + places
     if len(NOTIFIERS)!=0:
         logger.info("Alerting using Notifires")
         apobj.notify(
@@ -121,6 +125,90 @@ def alarm_off():
 def is_test_alert(alert):
     # if includes, all alerts are treated as not test
     return INCLUDE_TEST_ALERTS == 'False' and ('בדיקה' in alert['data'] or 'בדיקה מחזורית' in alert['data'])
+
+
+def standardize_name(name):
+    return re.sub(r'[\(\)\'\"]+', '', name).strip()
+
+def load_lamas_data():
+    file_path = 'lamas.json'
+    github_url = "https://raw.githubusercontent.com/idodov/RedAlert/main/apps/red_alerts_israel/lamas.json"
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lamas_data = json.load(file)
+        logger.info("Lamas data loaded from local file")
+
+    except FileNotFoundError:
+        logger.error(f"Error: Lamas data file not found at {file_path}, attempting to download from GitHub")
+        lamas_data = download_lamas_data(github_url, file_path)
+
+    except json.JSONDecodeError:
+        logger.error("Error: Invalid JSON in Lamas data file, attempting to download from GitHub")
+        lamas_data = download_lamas_data(github_url, file_path)
+
+    except Exception as e:
+        logger.error(f"Error loading Lamas data from local file: {e}")
+        lamas_data = download_lamas_data(github_url, file_path)
+
+    if 'areas' not in lamas_data:
+        logger.error("Unexpected JSON structure in Lamas data")
+        return None
+
+    for area, cities in lamas_data['areas'].items():
+        if isinstance(cities, dict):
+            standardized_cities = set([
+                standardize_name(city) for city in cities.keys()
+            ])
+        else:
+            logger.error(f"Unexpected cities structure in area {area}: {cities}")
+            standardized_cities = set()
+
+        lamas_data['areas'][area] = standardized_cities
+
+    return lamas_data
+
+def download_lamas_data(url, file_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        lamas_data = response.json()
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(lamas_data, file, ensure_ascii=False, indent=2)
+        logger.info(f"Lamas data downloaded from GitHub and saved to {file_path}")
+        return lamas_data
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error downloading Lamas data from GitHub: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        logger.error(f"Error decoding Lamas data from GitHub: {e}")
+        return None
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        return None
+
+
+def categorize_places(data, places):
+    categorized_places = {}
+    
+    for place in places:
+        for area, area_places in data['areas'].items():
+            if place in area_places:
+                if area not in categorized_places:
+                    categorized_places[area] = []
+                categorized_places[area].append(place)
+                break
+    
+    return categorized_places
+
+def format_output(categorized_places):
+    result = []
+    for area, places in categorized_places.items():
+        result.append(f"*ישובי {area}*:")
+        for place in places:
+            result.append(place)
+    return "\r\n".join(result)
+
 
 def monitor():
   #start the timer
@@ -145,4 +233,5 @@ def monitor():
      r.release_conn()
 
 if __name__ == '__main__':
-   monitor()
+    lamas = load_lamas_data()
+    monitor()
